0ae9a446a1ce0793a760a0ac79a2135eda92bbde
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 02:12:44 +0800

    Update news data on 2025-03-08T18:12:44.738Z

--- js/news.js
+++ js/news.js
@@ -1,18 +1,1 @@
-const news = [
-  {
-    "id": "hjgtWDXUIXhelPtZ4YAk",
-    "image": "",
-    "title": "測試訊息",
-    "description": "",
-    "content": "這是內文",
-    "updatetime": "2025-03-07T13:08:04.465Z"
-  },
-  {
-    "id": "6HfF7MaY4f3X6XrI9GAW",
-    "image": "img/news-default.jpg",
-    "title": "測試訊息12",
-    "description": 123456,
-    "content": 33333,
-    "updatetime": "2025-03-07T04:40:08.304Z"
-  }
-];
+const news = [];
