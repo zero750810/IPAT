d272adab348f16c22608406a4cd54c6376ff5b56
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-08 22:18:58 +0800

    Update course data on 2025-03-08T14:18:58.411Z

--- js/course.js
+++ js/course.js
@@ -1,1 +1,11 @@
-const course = [];
+const course = [
+  {
+    "add": "????",
+    "updatetime": "2025-03-07T13:32:29.497Z",
+    "title": "????",
+    "date": "10?30 1200?1300",
+    "price": 500,
+    "teacher": "????",
+    "content": "??"
+  }
+];
