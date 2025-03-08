bc2bf060f2e0015047166bafb2d23866724fd12e
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 01:53:16 +0800

    Update member data on 2025-03-08T17:53:16.272Z

--- js/member.js
+++ js/member.js
@@ -1,16 +1,8 @@
 const member = [
   {
     "id": "fZKGVxSc9WiJuzKVO8aI",
-    "image": "img/member-default.jpg",
-    "phone": "測試電話",
-    "updatetime": "2025-03-07T06:27:52.289Z",
-    "mail": "zero0810@gmail.com",
-    "introduction": "會員內文介紹",
+    "tag": "監事,一般會員,授課講師",
     "name": "測試會員",
-    "tag": [
-      "監事",
-      "一般會員",
-      "授課講師"
-    ]
+    "introduction": "會員內文介紹"
   }
 ];
