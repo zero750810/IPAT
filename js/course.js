fd8954f81c459c8a198e3a1e4a1bd7b445741bc7
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 02:45:45 +0800

    Update course data on 2025-03-08T18:45:45.777Z

--- js/course.js
+++ js/course.js
@@ -1,29 +1,6 @@
 const course = [
   {
-    "id": "",
-    "docId": "CjQxhrLJLlBwPMzstpmJ",
-    "title": "課程測試",
-    "content": "內文",
-    "teacher": "測試老師",
-    "add": "測試地址",
-    "price": 500,
-    "date": "10月30 1200～1300",
-    "updatetime": "2025-03-07T13:32:29.497Z"
-  },
-  {
-    "id": "",
-    "docId": "CjQxhrLJLlBwPMzstpmJ",
-    "title": "課程測試",
-    "content": "內文",
-    "teacher": "測試老師",
-    "add": "測試地址",
-    "price": 500,
-    "date": "10月30 1200～1300",
-    "updatetime": "2025-03-07T13:32:29.497Z"
-  },
-  {
-    "id": "",
-    "docId": "CjQxhrLJLlBwPMzstpmJ",
+    "id": "CjQxhrLJLlBwPMzstpmJ",
     "title": "課程測試",
     "content": "內文",
     "teacher": "測試老師",
