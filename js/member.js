483147570fcfcaedddd782680c69211e71559d29
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-08 22:48:38 +0800

    Update member data on 2025-03-08T14:48:37.979Z

--- js/member.js
+++ js/member.js
@@ -1,11 +1,16 @@
 const member = [
   {
-    "mail": "zero0810@gmail.com",
-    "updatetime": "2025-03-07T06:27:52.289Z",
-    "name": "測試會員",
-    "tag": "",
-    "introduction": "會員內文介紹",
+    "id": "fZKGVxSc9WiJuzKVO8aI",
     "image": "img/member-default.jpg",
-    "phone": "測試電話"
+    "phone": "測試電話",
+    "updatetime": "2025-03-07T06:27:52.289Z",
+    "mail": "zero0810@gmail.com",
+    "introduction": "會員內文介紹",
+    "name": "測試會員",
+    "tag": [
+      "監事",
+      "一般會員",
+      "授課講師"
+    ]
   }
 ];
