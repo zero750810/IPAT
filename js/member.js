bbb1d4b82271e244328ac68aa2c8638c694a0df2
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 02:41:08 +0800

    Update member data on 2025-03-08T18:41:08.469Z

--- js/member.js
+++ js/member.js
@@ -6,5 +6,13 @@ const member = [
     "tag": "監事,一般會員,授課講師",
     "updatetime": "2025-03-07T06:27:52.289Z",
     "introduction": "會員內文介紹"
+  },
+  {
+    "id": "",
+    "docId": "fZKGVxSc9WiJuzKVO8aI",
+    "name": "測試會員",
+    "tag": "監事,一般會員,授課講師",
+    "updatetime": "2025-03-07T06:27:52.289Z",
+    "introduction": "會員內文介紹"
   }
 ];
