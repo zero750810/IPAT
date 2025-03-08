b8d0da9bb1a07150779a45d14c9731677d0bac76
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-08 22:24:21 +0800

    Update member data on 2025-03-08T14:24:21.052Z

---
+++ js/member.js
@@ -0,0 +1,11 @@
+const member = [
+  {
+    "mail": "zero0810@gmail.com",
+    "updatetime": "2025-03-07T06:27:52.289Z",
+    "name": "測試會員",
+    "tag": "",
+    "introduction": "會員內文介紹",
+    "image": "img/member-default.jpg",
+    "phone": "測試電話"
+  }
+];
