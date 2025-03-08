b702a89db6852d7c52458de7b99354d5dc924793
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 02:45:44 +0800

    Update news data on 2025-03-08T18:45:44.335Z

--- js/news.js
+++ js/news.js
@@ -1,7 +1,6 @@
 const news = [
   {
-    "id": "",
-    "docId": "hjgtWDXUIXhelPtZ4YAk",
+    "id": "hjgtWDXUIXhelPtZ4YAk",
     "description": "",
     "image": "",
     "title": "測試訊息",
@@ -9,44 +8,7 @@ const news = [
     "updatetime": "2025-03-07T13:08:04.465Z"
   },
   {
-    "id": "",
-    "docId": "6HfF7MaY4f3X6XrI9GAW",
-    "description": 123456,
-    "image": "img/news-default.jpg",
-    "title": "測試訊息12",
-    "content": 33333,
-    "updatetime": "2025-03-07T04:40:08.304Z"
-  },
-  {
-    "id": "",
-    "docId": "hjgtWDXUIXhelPtZ4YAk",
-    "description": "",
-    "image": "",
-    "title": "測試訊息",
-    "content": "這是內文",
-    "updatetime": "2025-03-07T13:08:04.465Z"
-  },
-  {
-    "id": "",
-    "docId": "6HfF7MaY4f3X6XrI9GAW",
-    "description": 123456,
-    "image": "img/news-default.jpg",
-    "title": "測試訊息12",
-    "content": 33333,
-    "updatetime": "2025-03-07T04:40:08.304Z"
-  },
-  {
-    "id": "",
-    "docId": "hjgtWDXUIXhelPtZ4YAk",
-    "description": "",
-    "image": "",
-    "title": "測試訊息",
-    "content": "這是內文",
-    "updatetime": "2025-03-07T13:08:04.465Z"
-  },
-  {
-    "id": "",
-    "docId": "6HfF7MaY4f3X6XrI9GAW",
+    "id": "6HfF7MaY4f3X6XrI9GAW",
     "description": 123456,
     "image": "img/news-default.jpg",
     "title": "測試訊息12",
