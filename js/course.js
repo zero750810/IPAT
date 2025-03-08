1b2af06fe58e267f969ff754be4b4d268f323c3b
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-08 21:47:22 +0800

    Update course data on 2025-03-08T13:47:22.553Z

---
+++ js/course.js
@@ -0,0 +1,25 @@
+const course = [
+  {
+    "title": {
+      "stringValue": "????"
+    },
+    "teacher": {
+      "stringValue": "????"
+    },
+    "content": {
+      "stringValue": "??"
+    },
+    "price": {
+      "integerValue": "500"
+    },
+    "add": {
+      "stringValue": "????"
+    },
+    "date": {
+      "stringValue": "10?30 1200?1300"
+    },
+    "updatetime": {
+      "timestampValue": "2025-03-07T13:32:29.497Z"
+    }
+  }
+];
