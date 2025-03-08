c2630912267ce3a0673d943fe0babe59b05fed3d
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-08 21:47:20 +0800

    Update news data on 2025-03-08T13:47:20.580Z

---
+++ js/news.js
@@ -0,0 +1,13 @@
+const news = [
+  {
+    "updatetime": {
+      "timestampValue": "2025-03-07T13:08:04.465Z"
+    },
+    "content": {
+      "stringValue": "????"
+    },
+    "title": {
+      "stringValue": "????"
+    }
+  }
+];
