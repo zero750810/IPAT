2d549481d0ebbaedd527393299e923d2ffbd1422
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 20:41:20 +0800

    Update course data on 2025-03-09T12:41:20.173Z

---
+++ js/course.js
@@ -0,0 +1,15 @@
+
+const course = [
+  {
+    "id": "CjQxhrLJLlBwPMzstpmJ",
+    "title": "課程測試",
+    "content": "內文",
+    "teacher": "測試老師",
+    "add": "測試地址",
+    "price": 500,
+    "date": "10月30 1200～1300",
+    "updatetime": "2025-03-08T13:32:29.497Z"
+  }
+];
+window.course = course;
+  
