dbac69378211792e1c79432e625ac5a9874ace40
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-09 20:41:21 +0800

    Update member data on 2025-03-09T12:41:20.931Z

---
+++ js/member.js
@@ -0,0 +1,15 @@
+
+const member = [
+  {
+    "name": "測試會員",
+    "introduction": "會員內文介紹",
+    "tag": [
+      "監事",
+      "一般會員",
+      "授課講師"
+    ],
+    "updatetime": "2025-03-08T06:27:52.289Z"
+  }
+];
+window.member = member;
+  
