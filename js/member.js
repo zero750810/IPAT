32b4ac74021f3d3836941de83966d2758ef7a824
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-11 11:39:18 +0800

    Update member data on 2025-03-11T03:39:17.939Z

--- js/member.js
+++ js/member.js
@@ -1,16 +1,17 @@
 
-const member = [
+  const member = [
   {
     "id": "fZKGVxSc9WiJuzKVO8aI",
     "name": "測試會員",
     "introduction": "會員內文介紹",
     "tag": [
-      "監事",
+      "理事",
       "一般會員",
       "授課講師"
     ],
-    "updatetime": "2025-03-09T06:27:52.289Z"
+    "updatetime": "2025-03-10T03:37:07.170Z",
+    "urls": []
   }
 ];
-window.member = member;
+  window.member = member;
   
