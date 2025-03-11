6a47b6342b8550a40a95c9a5bfdc3ac989d7ec1a
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-11 11:39:17 +0800

    Update course data on 2025-03-11T03:39:17.216Z

--- js/course.js
+++ js/course.js
@@ -1,5 +1,19 @@
 
-const course = [
+  const course = [
+  {
+    "id": "pRAEM6k0svJR3PqGTkaa",
+    "title": "新增課程",
+    "content": "",
+    "teacher": "測試會員",
+    "add": "",
+    "price": 150,
+    "startdate": "2025-03-10T01:21:00.000Z",
+    "enddate": "2025-03-10T13:21:00.000Z",
+    "updatetime": "2025-03-11T03:37:24.051Z",
+    "urls": [
+      "pdf|||https://learning.nccu.edu.tw/sites/default/files/announcement/%E9%99%84%E4%BB%B6%E5%9B%9B%E6%95%99%E5%AD%B8%E5%A4%A7%E7%B6%B1%E7%AF%84%E4%BE%8B.pdf"
+    ]
+  },
   {
     "id": "CjQxhrLJLlBwPMzstpmS",
     "title": "課程測試1",
@@ -9,19 +23,21 @@ const course = [
     "price": 500,
     "startdate": "2025-03-08T13:32:29.497Z",
     "enddate": "2025-03-10T17:32:29.497Z",
-    "updatetime": "2025-03-08T13:32:29.497Z"
+    "updatetime": "2025-03-10T13:32:29.497Z",
+    "urls": []
   },
   {
     "id": "CjQxhrLJLlBwPMzstpmJ",
-    "title": "課程測試",
+    "title": "編輯課程",
     "content": "內文",
-    "teacher": "測試老師",
+    "teacher": "測試會員",
     "add": "測試地址",
     "price": 500,
-    "startdate": "2025-03-08T13:32:29.497Z",
-    "enddate": "2025-03-08T17:32:29.497Z",
-    "updatetime": "2025-03-08T13:32:29.497Z"
+    "startdate": "2025-03-10T01:22:00.000Z",
+    "enddate": "2025-03-10T01:29:00.000Z",
+    "updatetime": "2025-03-11T03:37:18.658Z",
+    "urls": []
   }
 ];
-window.course = course;
+  window.course = course;
   
