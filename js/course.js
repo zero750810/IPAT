2f96d324c8017c3b886e6aca81dd1cfc37243cd8
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-12 20:12:50 +0800

    Update course data on 2025-03-12T12:12:50.702Z

--- js/course.js
+++ js/course.js
@@ -1,5 +1,5 @@
 
-const course = [
+  const course = [
   {
     "id": "pRAEM6k0svJR3PqGTkaa",
     "title": "新增課程",
@@ -9,34 +9,26 @@ const course = [
     "price": 150,
     "startdate": "2025-03-10T01:21:00.000Z",
     "enddate": "2025-03-10T13:21:00.000Z",
-    "updatetime": "2025-03-11T03:37:24.051Z",
+    "updatetime": "2025-03-11T04:29:47.894Z",
     "urls": [
       "pdf|||https://learning.nccu.edu.tw/sites/default/files/announcement/%E9%99%84%E4%BB%B6%E5%9B%9B%E6%95%99%E5%AD%B8%E5%A4%A7%E7%B6%B1%E7%AF%84%E4%BE%8B.pdf"
-    ]
-  },
-  {
-    "id": "CjQxhrLJLlBwPMzstpmS",
-    "title": "課程測試1",
-    "content": "內文1",
-    "teacher": "測試老師",
-    "location": "測試地址1",
-    "price": 500,
-    "startdate": "2025-03-08T13:32:29.497Z",
-    "enddate": "2025-03-10T17:32:29.497Z",
-    "updatetime": "2025-03-11T13:32:29.497Z",
-    "urls": []
+    ],
+    "active": "",
+    "capacity": ""
   },
   {
     "id": "CjQxhrLJLlBwPMzstpmJ",
     "title": "編輯課程",
     "content": "內文",
     "teacher": "測試會員",
-    "location": "測試地址",
+    "location": "ipat總部",
     "price": 500,
     "startdate": "2025-03-10T01:22:00.000Z",
     "enddate": "2025-03-10T01:29:00.000Z",
-    "updatetime": "2025-03-11T03:37:18.658Z",
-    "urls": []
+    "updatetime": "2025-03-11T04:59:02.334Z",
+    "urls": [],
+    "active": true,
+    "capacity": ""
   }
 ];
   window.course = course;
