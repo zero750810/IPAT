f61f4084a3f448a1c7cccd0e43fd16f6526aa46d
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-25 00:16:21 +0800

    Update course data on 2025-03-24T16:16:21.485Z

--- js/course.js
+++ js/course.js
@@ -1,34 +1,18 @@
 
   const course = [
   {
-    "id": "pRAEM6k0svJR3PqGTkaa",
-    "title": "新增課程",
+    "id": "YxtX44f7Aq3rZO4jnvSM",
+    "title": "橡皮筋發射遊藝",
     "content": "",
-    "teacher": "測試會員",
-    "location": "",
-    "price": 150,
-    "startdate": "2025-03-10T01:21:00.000Z",
-    "enddate": "2025-03-10T13:21:00.000Z",
-    "updatetime": "2025-03-11T04:29:47.894Z",
-    "urls": [
-      "pdf|||https://learning.nccu.edu.tw/sites/default/files/announcement/%E9%99%84%E4%BB%B6%E5%9B%9B%E6%95%99%E5%AD%B8%E5%A4%A7%E7%B6%B1%E7%AF%84%E4%BE%8B.pdf"
-    ],
-    "active": "",
-    "capacity": ""
-  },
-  {
-    "id": "CjQxhrLJLlBwPMzstpmJ",
-    "title": "編輯課程",
-    "content": "內文",
-    "teacher": "測試會員",
-    "location": "ipat總部",
-    "price": 500,
-    "startdate": "2025-03-10T01:22:00.000Z",
-    "enddate": "2025-03-10T01:29:00.000Z",
-    "updatetime": "2025-03-11T04:59:02.334Z",
+    "teacher": "張宇文",
+    "location": "台北市大安區東豐街1號2樓",
+    "price": 0,
+    "startdate": "2025-04-19T01:00:00.000Z",
+    "enddate": "2025-04-19T04:00:00.000Z",
+    "updatetime": "2025-03-24T11:24:16.960Z",
     "urls": [],
     "active": true,
-    "capacity": ""
+    "capacity": 25
   }
 ];
   window.course = course;
