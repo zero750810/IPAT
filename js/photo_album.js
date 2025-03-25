1e94c32eaa95fe060c67d5e5c662f18d71cdc821
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-03-25 11:05:44 +0800

    Update photo_album data on 2025-03-25T03:05:44.068Z

--- js/photo_album.js
+++ js/photo_album.js
@@ -1,23 +1,4 @@
 
-  const photo_album = [
-  {
-    "id": "Ac8MvV59J7pJ8phA5h1B",
-    "title": "測試花絮",
-    "url": "https://www.facebook.com/media/set/?set=a.423061633273513&type=3",
-    "updatetime": "2025-03-11T12:07:26.443Z"
-  },
-  {
-    "id": "yQI9wVwW8ijZSIqkBOZk",
-    "title": "測試花絮134",
-    "url": "https://www.facebook.com/media/set/?set=a.423061633273513&type=3",
-    "updatetime": "2025-03-12T05:20:44.314Z"
-  },
-  {
-    "id": "QyshhlUyiHBFnmyFgw2n",
-    "title": "eadsad",
-    "url": "https://www.facebook.com/media/set/?set=a.423061633273513&type=3",
-    "updatetime": "2025-03-12T05:21:34.590Z"
-  }
-];
+  const photo_album = [];
   window.photo_album = photo_album;
   
