89651db4f319cbce4fde6126c8d78b2d38d7a920
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-05-07 19:45:05 +0800

    Update photo_album data on 2025-05-07T11:45:05.712Z

--- js/photo_album.js
+++ js/photo_album.js
@@ -1,4 +1,11 @@
 
-  const photo_album = [];
+  const photo_album = [
+  {
+    "id": "iFhnLYAoqosf0u2C5FsR",
+    "title": "測試花絮",
+    "url": "https://www.facebook.com/media/set/?set=a.423061633273513&type=3",
+    "updatetime": "2025-05-07T11:37:55.497Z"
+  }
+];
   window.photo_album = photo_album;
   
