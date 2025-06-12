b5a8f2b9a6233951f39790ab52f3b1756c485bab
zero750810 <60871544+zero750810@users.noreply.github.com>
2025-06-13 00:16:19 +0800

    Update course data on 2025-06-12T16:16:19.145Z

--- js/course.js
+++ js/course.js
@@ -1,6 +1,20 @@
 
   const course = [
   {
+    "id": "wKCT7GbyLSNMdbivecfV",
+    "title": "百變造型氣球&氣球互動遊戲",
+    "content": "",
+    "teacher": "張宇文",
+    "location": "台北市萬華區國興路5號(萬華親子館)",
+    "price": 0,
+    "startdate": "2025-06-28T05:00:00.000Z",
+    "enddate": "2025-06-28T08:00:00.000Z",
+    "updatetime": "2025-06-12T12:26:59.068Z",
+    "urls": [],
+    "active": true,
+    "capacity": 20
+  },
+  {
     "id": "YxtX44f7Aq3rZO4jnvSM",
     "title": "橡皮筋發射遊藝",
     "content": "",
